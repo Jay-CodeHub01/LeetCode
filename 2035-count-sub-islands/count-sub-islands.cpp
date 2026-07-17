89e9ff48c7f3afa568546f4aec58a3390683a356
Jay Patel <pateljay18v@gmail.com>
2026-07-17 15:17:25 +0530

    Time: 23 ms (88.51%) | Memory: 96.4 MB (59.13%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.cpp
+++ 2035-count-sub-islands/count-sub-islands.cpp
@@ -2,45 +2,35 @@ class Solution {
 public:
      int m, n;
     
-    void bfs(vector<vector<int>>& grid2, int i, int j) {
-        queue<pair<int,int>> q;
-        q.push({i,j});
-        grid2[i][j] = 0;
+    bool dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int i, int j) {
+        if(i < 0 || j < 0 || i >= m || j >= n || grid2[i][j] == 0)
+            return true; // no violation here
         
-        int dirs[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
+        grid2[i][j] = 0; // mark visited
         
-        while(!q.empty()) {
-            auto [x,y] = q.front(); q.pop();
-            for(auto& d : dirs) {
-                int nx = x + d[0], ny = y + d[1];
-                if(nx>=0 && ny>=0 && nx<m && ny<n && grid2[nx][ny]==1) {
-                    grid2[nx][ny] = 0;
-                    q.push({nx,ny});
-                }
-            }
-        }
+        // Check if this cell is valid in grid1
+        bool isSub = (grid1[i][j] == 1);
+        
+        // Explore neighbors
+        bool down  = dfs(grid1, grid2, i+1, j);
+        bool up    = dfs(grid1, grid2, i-1, j);
+        bool right = dfs(grid1, grid2, i, j+1);
+        bool left  = dfs(grid1, grid2, i, j-1);
+        
+        return isSub && down && up && right && left;
     }
     
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
         m = grid1.size();
         n = grid1[0].size();
-        
-        // Step 1: eliminate invalid land
-        for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
-                if(grid2[i][j]==1 && grid1[i][j]==0) {
-                    bfs(grid2, i, j); // wipe out this island
-                }
-            }
-        }
-        
-        // Step 2: count remaining islands
         int count = 0;
-        for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
-                if(grid2[i][j]==1) {
-                    bfs(grid2, i, j);
-                    count++;
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid2[i][j] == 1) {
+                    if(dfs(grid1, grid2, i, j)) {
+                        count++;
+                    }
                 }
             }
         }
