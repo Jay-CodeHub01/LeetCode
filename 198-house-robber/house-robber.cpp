654231315a8c707166799f9fe0f3851d464d35cc
Jay Patel <pateljay18v@gmail.com>
2026-08-09 13:54:37 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (79.68%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
-    int find(int index, vector<int>&nums, int n, vector<int>&dp){
-        if(index >= n) return 0;
-        if(dp[index] != -1) return dp[index];
+    // int find(int index, vector<int>&nums, int n, vector<int>&dp){
+    //     if(index >= n) return 0;
+    //     if(dp[index] != -1) return dp[index];
 
-        dp[index] = max(nums[index]+find(index+2 , nums, n, dp) , find(index+1 , nums, n, dp));
-        return dp[index];
-    }
+    //     dp[index] = max(nums[index]+find(index+2 , nums, n, dp) , find(index+1 , nums, n, dp));
+    //     return dp[index];
+    // }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n+2, -1);
-        return find(0, nums, n, dp);
+        // vector<int>dp(n+2, -1);
+        // return find(0, nums, n, dp);
+
+        // space optimization: 
+        int first = 0;
+        int second = 0;
+        int third;
+
+        for(int i=n-1 ; i>=0; i--){
+            third = max(nums[i]+second , first);
+            second = first;
+            first = third;
+        }
+
+        return third;
     }
 };
